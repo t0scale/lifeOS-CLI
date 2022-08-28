31faa58bd60edd9d47059c41ef6af237f3a7b80d
unknown <carlos.mendez.roca@gmail.com>
2022-08-27 18:18:47 -0700

    finished coding new function to update non current data oura data

--- utils/dateQuestion.js
+++ utils/dateQuestion.js
@@ -4,13 +4,17 @@ inquirer.registerPrompt("datetime", require("inquirer-datepicker-prompt"));
 exports.dateQuestion = [
   {
     type: "datetime",
-    name: "date",
-    message: "For which date would you like to pull Sleep Data (notion date)?",
-    format: ["mm", "-", "dd", "-", "yyyy"],
-    max: {
-      year: new Date().getFullYear(),
-      month: new Date().getMonth()+1,
-      day: new Date().getDate()
-    }
+    name: "reqdate",
+    message:
+      "For which date would you like to pull Sleep Data? (Use the notion date)",
+    format: ["yyyy", "-", "mm", "-", "dd"],
+    filter: (dt) => {
+      var doura = new Date(dt); // set date for oura as user input new date object
+      doura.setDate(doura.getDate() - 1); // reduce by 1 day
+      doura = doura.toISOString().replace(/T.*/,'') // convert format of date
+      dt = dt.toISOString().replace(/T.*/,'')
+      dates = [doura, dt]
+      return dates;
+    },
   },
 ];
